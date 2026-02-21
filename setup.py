5053cf22e3ffe58193d94553b9221abda8da9907
Vaishnavi V <vv1366315@gmail.com>
2026-02-21 12:15:27 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Vaishnavi V",
+    author_email="vv1366315@gmail.com",
+    packages=find_packages()
+)
